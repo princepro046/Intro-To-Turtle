9c94d76271a15036920df787ffc4217918abab8f
princepro046 <princepro046@gmail.com>
2024-12-19 20:14:50 +0500

    first commit

---
+++ Drawing With Turtle.py
@@ -0,0 +1,13 @@
+import turtle
+turtle.Screen().bgcolor("orange")
+turtle.Screen().setup(300,400)
+
+arham =turtle.Turtle()
+
+for i in range(1,5):
+    arham.forward(100)
+   
+    arham.right(90)
+
+    
+turtle.done()
---
+++ Turtle.py
@@ -0,0 +1,7 @@
+import turtle
+turtle.Screen().bgcolor("red")
+turtle.Screen().setup(300,400)
+
+
+
+turtle.done()
